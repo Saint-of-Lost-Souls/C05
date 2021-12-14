0bfa7a35a582cde513c04bfb8ac59fc9dfb2d22c
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 12:49:33 +1030

    update

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_recursive_factorial( int nb)
+{   
+    if (nb <= 0)  
+    {  
+        return 0;  // if arg is not valid
+    }  
+    else if (nb == 1)  
+    {  
+        return 1;  
+    }  
+    else   
+    {  
+        return nb * ft_recursive_factorial(nb-1);  
+    }  
+    return (nb);
+}  
+
+int main()
+{
+    
+    int result = ft_recursive_factorial(3);
+
+    printf("Factorial is %d", result);
+    return 0;
+}
---
+++ ex02/ft_interative_power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int pow)
+{
+    int result;
+
+    if (pow >= 0)
+    {
+        return (0);
+    }   
+    result = 1;
+    while (pow > 0)
+    {
+        result = result * nb;
+        pow--;
+    }
+    return (result);            
+}
+
+int main()
+{
+    printf("%d", ft_iterative_power(5, 4));
+    return 0;
+}
