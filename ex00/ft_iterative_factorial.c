69b1d1d1eac9e0cde38eba992ed6a3c6d0eb2cf6
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 10:59:33 +1030

    upload

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,23 @@
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+    {
+		return (0);
+    }
+	result = 1;
+	while (nb)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	
+	printf("%d\n", ft_iterative_factorial(5));
+}
